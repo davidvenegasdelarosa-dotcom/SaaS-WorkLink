cb4d88312811f2b418c0477f693388d5f0d2d14a
veneegaa <david.venegasdelarosa@alum.uca.es>
2026-04-07 23:34:36 +0200

    cambio en el repositorio

--- demo/src/main/java/com/example/demo/repository/bd_service.java
+++ demo/src/main/java/com/example/demo/repository/bd_service.java
@@ -6,5 +6,6 @@ import java.util.List;
 
 @Repository
 public interface bd_service extends Jparepository <Usuario, String>{
-
+    boolean existsByCorreo(String correo); //Comprobamos si el correo esta en la base de datos
+    boolean existsByCorreoAndPassword(String correo, String password); //Comprobamos si el correo y la contraseña son una tupla
 }
