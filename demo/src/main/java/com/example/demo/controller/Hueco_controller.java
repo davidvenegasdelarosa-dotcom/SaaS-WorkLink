218641039cbb997d72cf90f0c45e04ec09da20bd
veneegaa <david.venegasdelarosa@alum.uca.es>
2026-04-07 23:27:15 +0200

    base de datos creada y enlazada

---
+++ demo/src/main/java/com/example/demo/controller/Hueco_controller.java
@@ -0,0 +1,8 @@
+package com.example.demo.controller;
+import com.example.demo.modelos.Hueco;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class Hueco_controller {
+    @autowired
+}
