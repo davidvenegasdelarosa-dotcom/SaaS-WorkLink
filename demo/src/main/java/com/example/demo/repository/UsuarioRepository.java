e580abed6709b8ab44f8c6e8ef3c17adaf61c98d
veneegaa <david.venegasdelarosa@alum.uca.es>
2026-04-08 10:40:42 +0200

    error de repositorio de usuario corregido

--- demo/src/main/java/com/example/demo/repository/UsuarioRepository.java
+++ demo/src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -1,11 +1,11 @@
 package com.example.demo.repository;
 import com.example.demo.modelos.Usuario;
-import org.springframework.data.jpa.repository.Jparepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface bd_service extends Jparepository <Usuario, String>{
+public interface UsuarioRepository extends JpaRepository <Usuario, String>{
     boolean existsByCorreo(String correo); //Comprobamos si el correo esta en la base de datos
     boolean existsByCorreoAndPassword(String correo, String password); //Comprobamos si el correo y la contraseña son una tupla
 }
